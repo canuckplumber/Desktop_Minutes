278ff5d0d2b6b56ecdb0e1fc99450dfb28c7773d
Jaeson Cardiff <cardiffjaeson@gmail.com>
2026-06-14 19:48:03 -0600

    Add preload bridge

---
+++ src/preload/index.cjs
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("meetingApi", {
+  getConfig: () => ipcRenderer.invoke("config:get"),
+  createRealtimeToken: (options) =>
+    ipcRenderer.invoke("openai:create-realtime-token", options),
+  analyzeMeeting: (payload) => ipcRenderer.invoke("meeting:analyze", payload),
+  exportMinutes: (payload) => ipcRenderer.invoke("meeting:export", payload),
+});
